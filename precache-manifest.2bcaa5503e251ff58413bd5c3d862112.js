09b06bcdbdacfc4f5e8d8f31ea8c45f9d8f71f08
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-07-10 20:20:40 +0200

    Updates

---
+++ precache-manifest.2bcaa5503e251ff58413bd5c3d862112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cc3a0acd84a2d6335bf6c807869390",
+    "url": "/todos-list-react/index.html"
+  },
+  {
+    "revision": "26e17fb912a281d304e3",
+    "url": "/todos-list-react/static/css/main.9dad8fe5.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e17fb912a281d304e3",
+    "url": "/todos-list-react/static/js/main.22c9d878.chunk.js"
+  },
+  {
+    "revision": "97d8fa25e5bde6a122ac",
+    "url": "/todos-list-react/static/js/runtime-main.bb6efc59.js"
+  }
+]);
